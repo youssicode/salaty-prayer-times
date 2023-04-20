ab89b7aa58359182d2ace8ed2c5d2af9e9290922
youssef <youssimail@hotmail.com>
2023-04-20 05:31:31 +0000

    Add 'build'script in package.json+node_modules

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const app = express()
 app.use(express.static("frontEndStatic"))
 
 // Allow CORS for all routes
-app.use(cors())
+//* app.use(cors())
+const allowedOrigins = ['https://maps.googleapis.com', 'http://localhost:3000'];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
 
 // Render Root file
 app.get("/", (req, res) => {
@@ -23,9 +34,10 @@ app.get('/places', async (req, res) => {
     try {
         const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&type=mosque&rankby=distance&key=${googleMapApiKey}`;
         const response = await axios.get(url);
-
+        console.log(response.data) //!
         // res.setHeader('Access-Control-Allow-Origin', '*');
         res.json(response.data);
+        //! The usage of res.json(response.data): This line of code is sending the entire response.data as the response body to the client. Depending on the data returned by the Google Maps API, this may expose sensitive information to the client. It's recommended to sanitize the data and only send the necessary information to the client.
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
