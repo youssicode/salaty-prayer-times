d5205576785ce48656eda67ad59a91bbf70b2531
youssef <youssimail@hotmail.com>
2023-05-05 21:04:02 +0100

    Adhan main functionalities finished@

--- frontEndStatic/js/adhanSettings.js
+++ frontEndStatic/js/adhanSettings.js
@@ -1,62 +1,89 @@
 //? Imported Modules
 //==================
-import { saveToLocalStorage, getDataFromLocalStorage } from "./saveToLocalStorage.js"
-import dom from "./domElements.js"
-
+import {
+  saveToLocalStorage,
+  getDataFromLocalStorage,
+} from "./saveToLocalStorage.js";
+import dom from "./domElements.js";
 
 //? Functions
 //===========
 //* Activate / Di-activate Call-To-Prayer feature
-export const loadAdhanSettings = ()=> {
-    const adhanSetting = getDataFromLocalStorage('adhanSetting')
-    if (!adhanSetting) {
-        const settings = [{fajr:'on',dhuhr:'on',asr:'on',maghrib:'on',ishaa:'on'}, 'alarmISactive']
-        saveToLocalStorage("adhanSetting", settings)
-    } else {
-        if (adhanSetting[1] === 'alarmISactive') {
-            for(const prayerName in adhanSetting[0]) {
-                if (Object.hasOwnProperty.call(adhanSetting[0], prayerName)) {
-                    if(adhanSetting[0][prayerName] == 'off') {
-                        const adhanBell = document.querySelector(`[data-prayer="${prayerName}"]`)
-                        adhanBell.classList.add("prayerTimeCard__adhan--disabled")
-                    }
-                    if(adhanSetting[0][prayerName] == 'on') {
-                        const adhanBell = document.querySelector(`[data-prayer="${prayerName}"]`)
-                        adhanBell.classList.remove("prayerTimeCard__adhan--disabled")
-                    }
-                }
-            }
-            dom().adhanBells.forEach(el => {
-                el.addEventListener("click", function () {
-                    this.classList.toggle("prayerTimeCard__adhan--disabled")
-                    const settings = [{},'alarmISactive']
-                    dom().adhanBells.forEach(bell => {
-                        const prayer_name = bell.dataset.prayer
-                        if (prayer_name) { // to avoid the element related to 'sunrise'
-                            if (bell.classList.contains("prayerTimeCard__adhan--disabled")) {
-                                settings[0][prayer_name] = "off"
-                            } else {
-                                settings[0][prayer_name] = "on"
-                            }
-                        }
-                    })
-                    saveToLocalStorage("adhanSetting", settings)
-                })
-            })
-        } else {
-            dom().adhanBells.forEach(bell => bell.classList.add("prayerTimeCard__adhan--disabled"))
-            dom().activateAdhanSwitch.checked = true
-        }
-    }
-}
-
-export const adhanActivation = function() {
-    const adhanSetting = getDataFromLocalStorage('adhanSetting')
-    if (this.checked) { // if we use arrow function, 'this' won't refer to the element checked.
-        adhanSetting[1] = 'adhanDeactivated'
-    } else {
-        adhanSetting[1] = 'alarmISactive'
-    }
-    saveToLocalStorage("adhanSetting", adhanSetting)
-    loadAdhanSettings()
+export const loadAdhanSettings = () => {
+  const adhanSetting = getDataFromLocalStorage("adhanSetting");
+  if (!adhanSetting) {
+    initiateAdhanSettings();
+  } else {
+    renderAlarmIcons(adhanSetting);
   }
+  addEventListenerToAlarmIcons();
+};
+const initiateAdhanSettings = () => {
+  const settings = [
+    { fajr: "on", dhuhr: "on", asr: "on", maghrib: "on", ishaa: "on" },
+    "alarmISactive",
+  ];
+  saveToLocalStorage("adhanSetting", settings);
+  dom().activateAdhanSwitch.checked = false;
+};
+
+const renderAlarmIcons = (savedSetting) => {
+  if (savedSetting[1] == "alarmISactive") {
+    for (const prayerName in savedSetting[0]) {
+      if (Object.hasOwnProperty.call(savedSetting[0], prayerName)) {
+        if (savedSetting[0][prayerName] == "off") {
+          const adhanBell = document.querySelector(
+            `[data-prayer="${prayerName}"]`
+          );
+          adhanBell.classList.add("prayerTimeCard__adhan--disabled");
+        }
+        if (savedSetting[0][prayerName] == "on") {
+          const adhanBell = document.querySelector(
+            `[data-prayer="${prayerName}"]`
+          );
+          adhanBell.classList.remove("prayerTimeCard__adhan--disabled");
+        }
+      }
+    }
+  }
+
+  if (savedSetting[1] == "adhanDeactivated") {
+    dom().adhanBells.forEach((bell) =>
+      bell.classList.add("prayerTimeCard__adhan--disabled")
+    );
+    dom().activateAdhanSwitch.checked = true;
+  }
+};
+
+const addEventListenerToAlarmIcons = () => {
+  dom().adhanBells.forEach((bellBtn) => {
+    bellBtn.addEventListener("click", function () {
+      if (dom().activateAdhanSwitch.checked == true) return;
+      this.classList.toggle("prayerTimeCard__adhan--disabled");
+      const prayer = this.dataset.prayer;
+      saveAlarmsStates(prayer);
+    });
+  });
+};
+
+const saveAlarmsStates = (prayerName) => {
+  const adhanSetting = getDataFromLocalStorage("adhanSetting");
+  if (adhanSetting[0][prayerName] == "on") {
+    adhanSetting[0][prayerName] = "off";
+  } else {
+    adhanSetting[0][prayerName] = "on";
+  }
+  saveToLocalStorage("adhanSetting", adhanSetting);
+};
+
+export const adhanActivation = function () {
+  const adhanSetting = getDataFromLocalStorage("adhanSetting");
+  if (this.checked) {
+    // if we use arrow function, 'this' won't refer to the element checked.
+    adhanSetting[1] = "adhanDeactivated";
+  } else {
+    adhanSetting[1] = "alarmISactive";
+  }
+  saveToLocalStorage("adhanSetting", adhanSetting);
+  renderAlarmIcons(adhanSetting);
+};
--- frontEndStatic/js/index.js
+++ frontEndStatic/js/index.js
@@ -6,101 +6,120 @@ import { getUserCoordinates, autoLocateCity } from "./autoLocation.js";
 import { getPrayerTimes } from "./prayerTimesAPI.js";
 import { autoCompleteCitiesList } from "./autoCompleteCitiesList.js";
 import getNearbyMosquesList from "./nearbyMosques.js"; // default function
-import { saveToLocalStorage, getDataFromLocalStorage } from "./saveToLocalStorage.js";
-import { renderLocalTime, renderGregorianDate, renderFooterYear, hideLocationSearchWrapper, hideErrorMessage, hideNearbyMosques } from "./dataRendering.js";
+import {
+  saveToLocalStorage,
+  getDataFromLocalStorage,
+} from "./saveToLocalStorage.js";
+import {
+  renderLocalTime,
+  renderGregorianDate,
+  renderFooterYear,
+  hideLocationSearchWrapper,
+  hideErrorMessage,
+  hideNearbyMosques,
+} from "./dataRendering.js";
 import { toggleMenu } from "./burgerMenu.js";
 import { loadAdhanSettings, adhanActivation } from "./adhanSettings.js";
 import dom from "./domElements.js"; // default export
 
-
 //? Main Functions
 //================
 //* Get and display Islamic & Gregorian Dates
-const date = new Date()
-getIslamicDate(date)
-renderGregorianDate(date)
+const date = new Date();
+getIslamicDate(date);
+renderGregorianDate(date);
 
 //* Display local time
-let timeLoop
+let timeLoop;
 export const displayTime = (timezone) => {
-    clearInterval(timeLoop)
-    timeLoop = setInterval(() => {
-        const date = new Date();
-        const options = {
-            timeZone: timezone,
-            hour12: false,
-            hour: '2-digit',
-            minute: '2-digit'
-        };
-        const hour = date.getHours();
-        const time = hour === 0 ? date.toLocaleTimeString('en-US', options).replace(/24:/, '00:') : date.toLocaleTimeString('en-US', options);// X.replace(/24:/, '00:')=>replace '24'(midnight hour format) to '00', the ':' to avoid replacing minutes
-        renderLocalTime(time)
-    }, 1000)
-}
+  clearInterval(timeLoop);
+  timeLoop = setInterval(() => {
+    const date = new Date();
+    const options = {
+      timeZone: timezone,
+      hour12: false,
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    const hour = date.getHours();
+    const time =
+      hour === 0
+        ? date.toLocaleTimeString("en-US", options).replace(/24:/, "00:")
+        : date.toLocaleTimeString("en-US", options); // X.replace(/24:/, '00:')=>replace '24'(midnight hour format) to '00', the ':' to avoid replacing minutes
+    renderLocalTime(time);
+  }, 1000);
+};
 
 //* Get User Location Coordinates and then display the local time, adresse/city and prayer times
 async function loadData(time_zone) {
-    displayTime(time_zone)
-    const locationCoordinates = await getUserCoordinates()
-    if (locationCoordinates) {
-        saveToLocalStorage('locationCoordinates', locationCoordinates)
-        autoLocateCity(locationCoordinates)
-        getPrayerTimes(locationCoordinates)
-    }
+  displayTime(time_zone);
+  const locationCoordinates = await getUserCoordinates();
+  if (locationCoordinates) {
+    saveToLocalStorage("locationCoordinates", locationCoordinates);
+    autoLocateCity(locationCoordinates);
+    getPrayerTimes(locationCoordinates);
+  }
 }
 const local_time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-saveToLocalStorage('salaty_localTimeZone', local_time_zone)
-window.onload = ()=> {
-    loadData(local_time_zone)
-    loadAdhanSettings()
-}
+saveToLocalStorage("salaty_localTimeZone", local_time_zone);
+window.onload = () => {
+  loadData(local_time_zone);
+  loadAdhanSettings();
+};
+// Settings: Deactivate Adhan Alarm
+dom().activateAdhanSwitch.addEventListener("change", adhanActivation);
 
 //* Display/Hide city search component
 dom().locationBtn.addEventListener("mousedown", () => {
-    dom().locationSearchWrapper.classList.add("city-search-component-activated")
-    //* Auto-complete user entry with matched cities in Search component
-    dom().citySearchInput.addEventListener("input", autoCompleteCitiesList)
-    //* Manualy tirgger Auto-Location & Rendering Prayer Times functions
-    dom().autoLocateButton.addEventListener("click", () => {
-        hideErrorMessage()
-        const local_time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        saveToLocalStorage('salaty_localTimeZone', local_time_zone)
-        //* Refresh Current Gregorian Date
-        refreshGregorianDate(local_time_zone)
-        //* Refresh Current Hijri Date
-        getIslamicDate(new Date())
-        //* Refresh Current Time and Prayers Data        
-        loadData(local_time_zone)
-        hideLocationSearchWrapper()
-        hideNearbyMosques()
-    })
-})
+  dom().locationSearchWrapper.classList.add("city-search-component-activated");
+  //* Auto-complete user entry with matched cities in Search component
+  dom().citySearchInput.addEventListener("input", autoCompleteCitiesList);
+  //* Manualy tirgger Auto-Location & Rendering Prayer Times functions
+  dom().autoLocateButton.addEventListener("click", () => {
+    hideErrorMessage();
+    const local_time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    saveToLocalStorage("salaty_localTimeZone", local_time_zone);
+    //* Refresh Current Gregorian Date
+    refreshGregorianDate(local_time_zone);
+    //* Refresh Current Hijri Date
+    getIslamicDate(new Date());
+    //* Refresh Current Time and Prayers Data
+    loadData(local_time_zone);
+    hideLocationSearchWrapper();
+    hideNearbyMosques();
+  });
+});
 
-window.addEventListener("keydown", e => {
-    if (e.key === "Escape") {
-        hideLocationSearchWrapper()
-    }
-})
-window.addEventListener("click", e => {
-    // if the search component is hidden or the element we clicked on = (e.target) is child of "locationBtn" then return, if not, then execute hideLocationSearchWrapper()
-    if (!dom().locationSearchWrapper.classList.contains("city-search-component-activated") || dom().locationWrapper.contains(e.target)) return
-    hideLocationSearchWrapper()
-})
+window.addEventListener("click", (e) => {
+  // if the search component is hidden or the element we clicked on = (e.target) is child of "locationBtn" then return, if not, then execute hideLocationSearchWrapper()
+  if (
+    !dom().locationSearchWrapper.classList.contains(
+      "city-search-component-activated"
+    ) ||
+    dom().locationWrapper.contains(e.target)
+  )
+    return;
+  hideLocationSearchWrapper();
+});
 
 //* Render Nearby Mosques List according to current location
 dom().nearbyMosquesShowBtn.addEventListener("click", () => {
-    if (dom().nearbyMosquesSection.classList.contains("nearbyMosquesBtnClicked")) return
-    const coords = getDataFromLocalStorage("locationCoordinates")
-    getNearbyMosquesList(coords)
-})
+  if (dom().nearbyMosquesSection.classList.contains("nearbyMosquesBtnClicked"))
+    return;
+  const coords = getDataFromLocalStorage("locationCoordinates");
+  getNearbyMosquesList(coords);
+});
 
 //* Assign Year in the Footer Dinamically
-renderFooterYear(date.getFullYear())
+renderFooterYear(date.getFullYear());
 
 // Show/Hide DropDown Menu
-dom().menuBurgerBtn.onclick = toggleMenu
-dom().menuHideBtn.onclick = toggleMenu
+dom().menuBurgerBtn.onclick = toggleMenu;
+dom().menuHideBtn.onclick = toggleMenu;
 
-// Settings: Deactivate Adhan Alarm
-
-dom().activateAdhanSwitch.addEventListener('change',adhanActivation);
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    hideLocationSearchWrapper();
+    dom().dropDownMenu.classList.remove("visible");
+  }
+});
