9d9334acde77ca563eec93d4750bf9c7116ae784
Youssef <youssimail@hotmail.com>
2024-12-22 19:53:01 +0100

    ✔ Refactoring code

--- frontEndStatic/js/autoCompleteCitiesList.js
+++ frontEndStatic/js/autoCompleteCitiesList.js
@@ -13,31 +13,33 @@ let newCitiesResult = []
 export function autoCompleteCitiesList() {
   const userEntry = query(".search-for-city-input").value.trim()
   const citiesSearchResult = []
-
-  clearChildren(citiesListMatch)
-  // Auto-Complete function will run only after typing 2 characters or more
-  if (userEntry.length < 2) {
-    // Initialize the newCitiesResult array when there is new search
-    newCitiesResult.length = 0
-    return
+  const runningConditionOk = (userEntry) => {
+    if (userEntry.length < 2) {
+      newCitiesResult.length = 0
+      return false
+    }
+    return true
+  }
+  const serarchMatchedCities = (cities) => {
+    cities.forEach((city) => {
+      const renderMatchedCityName = (city) => {
+        const cityCountryName = document.createElement("LI")
+        cityCountryName.classList.add("extracted-cities-list__city")
+        cityCountryName.textContent = city
+        citiesListMatch.appendChild(cityCountryName)
+        addClickEventToSuggestedCity(cityCountryName, city)
+      }
+      const arrayTextMatch = city.slice(0, userEntry.length).toLowerCase()
+      if (arrayTextMatch === userEntry.toLowerCase()) {
+        renderMatchedCityName(city)
+        citiesSearchResult.push(city)
+      }
+    })
+    return citiesSearchResult
   }
 
+  clearChildren(citiesListMatch)
+  if (!runningConditionOk(userEntry)) return
   if (newCitiesResult.length === 0) newCitiesResult = citiesOfTheWorld
-
-  newCitiesResult.forEach((city) => {
-    const arrayTextMatch = city.slice(0, userEntry.length).toLowerCase()
-    if (arrayTextMatch === userEntry.toLowerCase()) {
-      renderMatchedCityName(city)
-      citiesSearchResult.push(city)
-    }
-  })
-  newCitiesResult = citiesSearchResult
-}
-
-const renderMatchedCityName = (city) => {
-  const cityCountryName = document.createElement("LI")
-  cityCountryName.classList.add("extracted-cities-list__city")
-  cityCountryName.textContent = city
-  citiesListMatch.appendChild(cityCountryName)
-  addClickEventToSuggestedCity(cityCountryName, city)
+  newCitiesResult = serarchMatchedCities(newCitiesResult)
 }
